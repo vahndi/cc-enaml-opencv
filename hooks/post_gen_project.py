fc10c011e1fddc100358e7bec99eb7149a3dcd6d
vahndi.minah <vahndi.minah@frogdesign.com>
2018-10-17 23:15:58 -0400

    subbed template variables into postgen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,34 +1,11 @@
 import subprocess
-from os import listdir, getcwd
-from os.path import isdir, isfile
-
-# find variables
-project_dir_name = [d for d in listdir(getcwd()) if isdir(d)][0]
-env_file_name = [f for f in listdir(getcwd()) if isfile(f)
-                 and f.startswith('environment_')][0]
-with open(env_file_name, 'r') as f:
-    env_name = f.readline().split(':')[1].strip()
-
-# set up environment
-subprocess.run(
-    args=[
-        '/anaconda3/bin/conda',
-        'env',
-        'create',
-        '-f',
-        'environment_osx.yml'
-    ]
-)
 
 # activate environment and run app
 commands = """
-conda activate %s
-python ./%s/run_%s.py
-""" % (
-    env_name,
-    project_dir_name,
-    project_dir_name
-)
+/anaconda3/bin/conda env create -f environment_{{cookiecutter.platform}}.yml
+conda activate {{cookiecutter.env_name}}
+python ./{{cookiecutter.project_name}}/run_{{cookiecutter.project_name}}.py
+"""
 subprocess.run(
     args=commands, shell=True
 )
