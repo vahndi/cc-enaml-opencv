62cf8a18291e262890c5ed31db89ed5378734c46
vahndi.minah <vahndi.minah@frogdesign.com>
2018-10-17 23:00:50 -0400

    updated post_gen_project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,31 @@
 import subprocess
+from os import listdir, getcwd
+from os.path import isdir, isfile
 
+project_dir_name = [d for d in listdir(getcwd()) if isdir(d)][0]
+env_file_name = [f for f in listdir(getcwd()) if isfile(f)
+                 and f.startswith('environment_')][0]
+
+# set up environment
 subprocess.run(
-    args=['conda', 'env', 'create', '-f', 'environment_{{cookiecutter.platform}}.yml'],
-    shell=True
+    args=[
+        '/anaconda3/bin/conda',
+        'env',
+        'create',
+        '-f',
+        'environment_osx.yml'
+    ]
+)
+
+# activate environment and run app
+commands = """
+conda activate %s
+python ./%s/run_%s.py
+""" % (
+    env_file_name,
+    project_dir_name,
+    project_dir_name
+)
+subprocess.run(
+    args=commands, shell=True
 )
