7022c15d7b10ad9dc170f8a2db9667cd0c96d851
vahndi.minah <vahndi.minah@frogdesign.com>
2018-10-17 23:07:03 -0400

    fixed env name

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,9 +2,12 @@ import subprocess
 from os import listdir, getcwd
 from os.path import isdir, isfile
 
+# find variables
 project_dir_name = [d for d in listdir(getcwd()) if isdir(d)][0]
 env_file_name = [f for f in listdir(getcwd()) if isfile(f)
                  and f.startswith('environment_')][0]
+with open(env_file_name, 'r') as f:
+    env_name = f.readline().split(':')[1].strip()
 
 # set up environment
 subprocess.run(
@@ -22,7 +25,7 @@ commands = """
 conda activate %s
 python ./%s/run_%s.py
 """ % (
-    env_file_name,
+    env_name,
     project_dir_name,
     project_dir_name
 )
